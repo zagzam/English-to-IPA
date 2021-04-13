c9a6cb9547ef470f85b5acccb1dbed1bd9f852ad
zagzam <zagzam@yahoo.com>
2021-04-13 11:47:23 +0800

    code updated to use IPA system

--- eng_to_ipa/__init__.py
+++ eng_to_ipa/__init__.py
@@ -1,6 +1,5 @@
 from .transcribe import *
-from .stress import *
 from .rhymes import *
-from .syllables import *
+from .normalize import *
 
-__all__ = ['transcribe', 'rhymes', 'stress', 'syllables']
+__all__ = ['transcribe', 'rhymes', 'normalize']
